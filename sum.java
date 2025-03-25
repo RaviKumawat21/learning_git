4a8022f46c25989196a8891e782cd5bc380bc715
Ravi Kumawat <ravikumawat8860@gmail.com>
2025-03-25 23:34:36 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,8 @@
+//write code for 5+6 and print the results
+public class sum{
+    public static void main(String[] args) {}
+    public static void sumFiveSix() {
+        int sum = 5 + 6;
+        System.out.println("The sum of 5 and 6 is: " + sum);
+    }
+}

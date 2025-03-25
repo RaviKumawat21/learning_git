0f86559cbfb5b22a1c8e2b929cf6bc4c73209772
Ravi Kumawat <ravikumawat8860@gmail.com>
2025-03-25 23:45:11 +0530

    third committ

--- sub.java
+++ sub.java
@@ -2,7 +2,7 @@ public class sub {
     public static void main(String[] args) {
         //write the cide for sub
         int subtraction = 5 - 3;
-        System.out.println("The result of subtraction is: " + subtraction);
+        System.out.println("The mu of subtraction is: " + subtraction);
     }
     
 }
--- sum.java
+++ sum.java
@@ -3,6 +3,6 @@ public class sum{
     public static void main(String[] args) {}
     public static void sumFiveSix() {
         int sum = 5 + 6;
-        System.out.println("The sum of 5 and 6 is: " + sum);
+        System.out.println("The sum of 5 and 6 is wonderful: " + sum);
     }
 }

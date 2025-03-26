b5538231d0b5a5af77dfb28373be8c266fc65a99
Ravi Kumawat <ravikumawat8860@gmail.com>
2025-03-26 15:30:16 +0530

    div.java added

---
+++ div.java
@@ -0,0 +1,8 @@
+public class div {
+    public static void main(String[] args) {
+        //divide
+        int division = 10 / 2;
+        System.out.println("The quotient of 10 and 2 is: " + division);
+    }
+    
+}

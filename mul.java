311168045e23d5bf1bba28d923ff49c65d12e52e
Ravi Kumawat <ravikumawat8860@gmail.com>
2025-03-26 14:45:00 +0530

    here in this code multiply

---
+++ mul.java
@@ -0,0 +1,7 @@
+public class mul {
+    public static void main(String[] args) {
+        //multiply
+        int multiplication = 5 * 3;
+        System.out.println("The product of 5 and 3 is: " + multiplication);
+    }
+}
